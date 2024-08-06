1369e62d6f3543816bec610dadb12791e1cf3302
bangnguyen2264 <2251120335@ut.edu.vn>
2024-08-06 10:32:20 +0700

    feat: add base entity

---
+++ src/main/java/com/example/springsecurity/model/entity/Account.java
@@ -0,0 +1,24 @@
+package com.example.springsecurity.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "accounts")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Account extends BaseEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+
+
+
+
+}
